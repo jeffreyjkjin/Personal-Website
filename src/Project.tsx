128a159187e6f160355950f851f29c7df0bae85f
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-05-11 19:00:44 -0700

    Update Project.
    -Created ProjectData interface to encapsulate data related to a project
    -Updated signature for Project function call to take a project as input
    -Updated sections of JSX to use data from the input project rather than hard coding information

--- src/Project.tsx
+++ src/Project.tsx
@@ -3,14 +3,24 @@ import { MdExpandLess, MdExpandMore } from "react-icons/Md"
 import { useState } from "react"
 import "./styles.css"
 
-export const Project: React.FC = () => {
+export interface ProjectData {
+    title: string; 
+    github?: string;
+    youtube?: string; 
+    image: string;
+    tools: string[];   
+    what: string;                
+    soWhat: string[];
+}
+
+export const Project: React.FC<ProjectData> = (project: ProjectData) => {
     const [open, setOpen] = useState<boolean>(false);
 
     return (
         <div className="flex flex-col font-metropolis">
             <div className="flex flex-grow text-2xl p-6 bg-grey">
                 <div className="font-bold grow">
-                    Project Name
+                    {project.title}
                 </div>
                 <div className="flex flex-row-reverse justify-end gap-4">
                     <div>
@@ -18,64 +28,66 @@ export const Project: React.FC = () => {
                             {!open ? <MdExpandMore />: <MdExpandLess />}
                         </button>
                     </div>
-                    <div>
-                        <a href="">
-                            <BsYoutube />
-                        </a>
-                    </div>
-                    <div>
-                        <a href="">
-                            <BsGithub />
-                        </a>
-                    </div>
+                    {project.github && (
+                        <div>
+                            <a href={project.github}>
+                                <BsGithub />
+                            </a>
+                        </div>
+                    )}
+                    {project.youtube && (
+                        <div>
+                            <a href={project.youtube}>
+                                <BsYoutube />
+                            </a>
+                        </div>
+                    )}
                 </div>
             </div>
             {open && (
-                    <div className="flex p-6 bg-grey">
-                        <div className="flex flex-col w-1/2">
-                            <div>
-                                <img alt="Project Image" />
+                <div className="flex p-6 bg-grey gap-6">
+                    <div className="flex flex-col w-1/2">
+                        <div>
+                            <img src={project.image} alt={`${project.title} Image`} />
+                        </div>
+                        <div className="flex flex-wrap justify-center gap-2 text-lg text-white">
+                            {project.tools.map((tool: string) => {
+                                return (
+                                    <div key={tool} className="bg-red px-2 py-1 my-2">
+                                        {tool}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-1/2 text-xl text-white gap-4">
+                        <div className="flex flex-col bg-blue py-6">
+                            <div className="font-bold text-center">
+                                The What
                             </div>
-                            <div className="flex flex-wrap justify-center gap-2 text-lg text-white">
-                                <div className="bg-red px-2 py-1">
-                                    Language
-                                </div>
-                                <div className="bg-red px-2 py-1">
-                                    Framework
-                                </div>
+                            <div className="text-justify text-lg px-6">
+                                {project.what}
                             </div>
                         </div>
-                        <div className="flex flex-col w-1/2 text-xl text-white gap-4">
-                            <div className="flex flex-col bg-blue py-6">
-                                <div className="font-bold text-center">
-                                    The What
-                                </div>
-                                <div className="text-justify text-lg px-6">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                </div>
+                        <div className="bg-blue py-6">
+                            <div className="font-bold text-center">
+                                The So What
                             </div>
-                            <div className="bg-blue py-6">
-                                <div className="font-bold text-center">
-                                    The So What
-                                </div>
-                                <div className="text-justify text-lg px-6">
-                                    <ul className="list-inside list-disc">
-                                        <li>
-                                            Item 1
-                                        </li>
-                                        <li>
-                                            Item 2
-                                        </li>
-                                        <li>
-                                            Item 3
-                                        </li>
-                                    </ul>
-                                </div>
+                            <div className="text-justify text-lg px-6">
+                                <ul className="list-inside list-disc">
+                                    {project.soWhat.map((item: string) => {
+                                        return (
+                                            <li key={item}>
+                                                {item}
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
                             </div>
                         </div>
                     </div>
-                )
-            }
+                </div>
+            )}
         </div>
     );
 }
