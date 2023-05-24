f8dd68f0839782b0b8fce32ea4cba2821ad627be
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-05-23 22:52:43 -0700

    Updated About, Contact, and Project.
    -Implemented logic to detect whether section is in the view or not
    -Modified components to use ScrollReveal

--- src/About.tsx
+++ src/About.tsx
@@ -1,26 +1,37 @@
-import { BsGithub, BsLinkedin } from "react-icons/Bs"
 import picture from "./assets/picture.jpg"
+import { BsGithub, BsLinkedin } from "react-icons/Bs"
+import { useInView } from "react-intersection-observer"
+import { ScrollReveal } from "./ScrollReveal.tsx"
 import "./styles.css"
 
 export const About: React.FC = () => {
+    const [ref, inView] = useInView({
+        threshold: 0.3,
+        initialInView: true
+    });
+
     return (
-        <div className="flex w-[56rem] space-x-10 my-[40vh] drop-shadow-lg">
-            <div className="flex flex-col font-metropolis bg-blue text-white text-lg w-1/2">
-                <p className="p-6 h-3/4 text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <div className="flex flex-col place-content-end p-6 h-1/4 text-xl">
-                    <div className="flex items-center gap-2">
-                        <BsLinkedin /> 
-                        <a href="https://www.linkedin.com/in/jeffreyjkjin">jeffreyjkjin</a>
+        <section ref={ref} className="w-[56rem] my-[40vh]">
+            <ScrollReveal inView={inView}>
+                <div className="flex space-x-10 drop-shadow-lg">
+                    <div className="flex flex-col font-metropolis bg-blue text-white text-lg w-1/2">
+                        <p className="p-6 h-3/4 text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <div className="flex flex-col place-content-end p-6 h-1/4 text-xl">
+                            <div className="flex items-center gap-2">
+                                <BsLinkedin /> 
+                                <a href="https://www.linkedin.com/in/jeffreyjkjin">jeffreyjkjin</a>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <BsGithub /> 
+                                <a href="https://www.github.com/jeffreyjkjin">jeffreyjkjin</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex items-center gap-2">
-                        <BsGithub /> 
-                        <a href="https://www.github.com/jeffreyjkjin">jeffreyjkjin</a>
-                    </div>
+                    <img src={picture} alt="me :)" className="w-1/2" />
                 </div>
-            </div>
-            <img src={picture} alt="me :)" className="w-1/2" />
-        </div>
+            </ScrollReveal>
+        </section>
     );
 }
--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,26 +1,37 @@
 import { MdOutlinePhone, MdMailOutline } from "react-icons/Md"
+import { useInView } from "react-intersection-observer"
+import { ScrollReveal } from "./ScrollReveal.tsx"
 import "./styles.css"
 
 export const Contact: React.FC = () => {
+    const [ref, inView] = useInView({
+        threshold: 0.3,
+        initialInView: true
+    });
+
     return (
-        <div className="flex flex-col w-[56rem] font-metropolis place-items-center gap-6 my-[40vh] drop-shadow-lg">
-            <p className="font-bold text-8xl">
-                Let's Chat!
-            </p>
-            <div className="flex text-white text-3xl space-x-10 w-[46rem]">
-                <div className="flex bg-blue place-content-center gap-2 p-6 w-1/2">
-                        <MdOutlinePhone />
-                        <a href="tel:778-938-6318">
-                            (778) 938-6318
-                        </a>
+        <section ref={ref} className="w-[56rem] my-[40vh]">
+            <ScrollReveal inView={inView}>
+                <div className="flex flex-col font-metropolis place-items-center gap-6 drop-shadow-lg">
+                    <p className="font-bold text-8xl">
+                        Let's Chat!
+                    </p>
+                    <div className="flex text-white text-3xl space-x-10 w-[46rem]">
+                        <div className="flex bg-blue place-content-center gap-2 p-6 w-1/2">
+                                <MdOutlinePhone />
+                                <a href="tel:778-938-6318">
+                                    (778) 938-6318
+                                </a>
+                        </div>
+                        <div className="flex bg-blue place-content-center gap-2 p-6 w-1/2">
+                                <MdMailOutline /> 
+                                <a href="mailto:jeffrey_jin@sfu.ca">
+                                    jeffrey_jin@sfu.ca
+                                </a>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex bg-blue place-content-center gap-2 p-6 w-1/2">
-                        <MdMailOutline /> 
-                        <a href="mailto:jeffrey_jin@sfu.ca">
-                            jeffrey_jin@sfu.ca
-                        </a>
-                </div>
-            </div>
-        </div>
+            </ScrollReveal>
+        </section>
     );
 }
