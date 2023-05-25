a8e9e117faf6a1429e02d7c4a4e8c5b14b05de7b
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-05-25 02:23:59 -0700

    Update Portfolio.tsx
    -Enclosed projects in another flex box

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -6,14 +6,16 @@ export const Portfolio: React.FC = () => {
     const projects: ProjectData[] = projectData;
 
     return (
-        <div className="flex flex-grow flex-col items-center w-[56rem] my-[40vh] gap-10 drop-shadow-lg">
-            {projects.map((proj: ProjectData) => {
-                return (
-                    <div key={proj.title} className="w-full">
-                        <Project {...proj} />
-                    </div>
-                );
-            })}
+        <div className="flex my-[40vh]">
+            <div className="flex flex-grow flex-col items-center w-[56rem] gap-10 drop-shadow-lg">
+                {projects.map((proj: ProjectData) => {
+                    return (
+                        <div key={proj.title} className="w-full">
+                            <Project {...proj} />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
