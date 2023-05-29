de282eacd9eb16691c45b009c74f9ff8b5a6f271
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-05-28 22:53:01 -0700

    Update Navbar.tsx
    -Added animation for highlighting name

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,40 +3,59 @@ import "./styles.css"
 import Typewriter, { TypewriterClass } from "typewriter-effect"
 
 export const Navbar: React.FC = () => {
-    const animation: AnimationControls = useAnimation();
+    const boxAnimation: AnimationControls = useAnimation();
+    const navAnimation: AnimationControls = useAnimation();
 
     return (
         <div className="flex flex-col font-metropolis justify-items-start h-screen w-[56rem] place-content-center drop-shadow-lg">
-            <h1 className="font-bold text-[5.7rem] leading-none">
+            <div className="flex font-bold text-[5.7rem] leading-none relative z-20">
                 <Typewriter 
                     options={{
                         delay: 100
                     }}
                     onInit={(typewriter: TypewriterClass) => {
                         typewriter
-                            .typeString("Hello friends! <br /> My name is Jeffrey.")
-                            .start()
-                            .callFunction(() => {
-                                animation.start({
-                                    y: 0,
-                                    opacity: 1,
-                                    transition: {
-                                        type: "spring",
-                                        duration: 1,
-                                        bounce: 0.3
-                                    }
-                                });
+                        .typeString("Hello friends! <br /> My name is Jeffrey.")
+                        .start()
+                        .callFunction(() => {
+                            navAnimation.start({
+                                y: 0,
+                                opacity: 1,
+                                transition: {
+                                    type: "spring",
+                                    duration: 1,
+                                    bounce: 0.3
+                                }
                             });
-                    }}
+                            boxAnimation.start({
+                                x: 0,
+                                opacity: 0.75,
+                                transition: {
+                                    type: "spring",
+                                    duration: 0.5,
+                                    bounce: 0.2
+                                }
+                            });
+                        });
+                        }}
                     />
-            </h1>
+                <motion.div 
+                    className="bg-lightblue w-[20rem] h-[4rem] absolute -bottom-[0.5rem] right-[1rem] -z-10"
+                    initial={{
+                        x: "20vw",
+                        opacity: 0
+                    }}
+                    animate={boxAnimation}
+                    >
+                </motion.div>
+            </div>
             <motion.div 
-                className="flex text-lg gap-16 m-2"
+                className="flex text-lg gap-16 m-2 relative"
                 initial={{
                     y: "10vh",
                     opacity: 0
                 }}
-                animate={animation}
+                animate={navAnimation}
                 >
                 <button>
                     About
