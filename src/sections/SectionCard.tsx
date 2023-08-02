95f1bc79556d75e7d6d3821b127ec8334c12e24d
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-01 22:33:23 -0700

    Update SectionCard.tsx
    -Added scroll animation for section card

--- src/sections/SectionCard.tsx
+++ src/sections/SectionCard.tsx
@@ -1,4 +1,7 @@
+import { useInView } from "react-intersection-observer"
+
 import "@/styles.css"
+import { ScrollReveal } from "@/animation/ScrollReveal.tsx";
 
 interface SectionCardProps {
     name: string;
@@ -11,16 +14,29 @@ interface SectionCardProps {
            length - The length of the highlight box.
 */
 export const SectionCard: React.FC<SectionCardProps> = ({ name, length }) => {
+    const [ref, inView] = useInView({
+        threshold: 0.5,
+        initialInView: true
+    });
+
     return (
-        <section>
+        <section ref={ref}>
             <div className="font-metropolis font-bold text-3xl drop-shadow-lg invisible lg:visible">
-                <div className="flex flex-row-reverse rotate-180 [writing-mode:vertical-rl] mx-6 gap-6">
-                    <p className="z-20">{name}</p>
-                    <div className="flex relative">
-                        <p className="-tracking-[0.2rem] relative">——————</p>
-                        <div className={`bg-lightblue absolute w-[1.5rem] h-[${length}rem] right-[0.9rem] top-[9.25rem]`}></div>
+                <ScrollReveal 
+                    inView={inView} 
+                    initial={{
+                        y: "40vh", 
+                        opacity: 0
+                    }}
+                    >
+                    <div className="flex flex-row-reverse rotate-180 [writing-mode:vertical-rl] mx-6 gap-6">
+                        <p className="z-20">{name}</p>
+                        <div className="flex relative">
+                            <p className="-tracking-[0.2rem] relative">——————</p>
+                            <div className={`bg-lightblue absolute w-[1.5rem] h-[${length}rem] right-[0.9rem] top-[9.25rem] -z-10`}></div>
+                        </div>
                     </div>
-                </div>
+                </ScrollReveal>
             </div>
         </section>
     );
