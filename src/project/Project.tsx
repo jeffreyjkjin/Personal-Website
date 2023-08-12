6fee424997cfd36780555bbcea781eb3ff2b91e4
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-12 16:57:26 -0700

    Fixed accordion disappearing when opening
    -Added an inView hook in portfolio which is passed to each project
    -Updated project to only animate when not in view anymore if portfolio is also not in view

--- src/project/Project.tsx
+++ src/project/Project.tsx
@@ -1,27 +1,42 @@
 import { ProjectData } from "@/project/ProjectData.tsx"
 import { ProjectDetails } from "@/project/ProjectDetails.tsx"
 import { ProjectTitle } from "@/project/ProjectTitle.tsx"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useInView } from "react-intersection-observer"
 import { ScrollReveal } from "@/animation/ScrollReveal.tsx"
 import "@/styles.css"
 
+interface ProjectProps {
+    project: ProjectData;
+    portfolioView: boolean;
+}
+
 /*
      DESC: An accordion that contains the details for a single project.
     PARAM: project - An individual project.
+           portfolioView - Indicates whether the parent (portfolio) is visible by the user.
 */
-export const Project: React.FC<ProjectData> = (project: ProjectData) => {
+export const Project: React.FC<ProjectProps> = ({ project, portfolioView }) => {
     const [open, setOpen] = useState<boolean>(false);
 
+
+    // checks if current project and portfolio section are both in view
     const [ref, inView] = useInView({
         threshold: 0.5,
         initialInView: true
     });
 
+    const [view, setView] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (inView && portfolioView) setView(true);
+        else if (!inView && !portfolioView) setView(false);
+    }, [inView, portfolioView]);
+
     return (
         <section ref={ref}>
             <ScrollReveal 
-                inView={inView} 
+                inView={view} 
                 animation={{
                     y: 0, 
                     transition: { 
--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -3,23 +3,30 @@ import { Project } from "@/project/Project.tsx"
 import { ProjectData } from "@/project/ProjectData.tsx"
 import "@/styles.css"
 
+import { useInView } from "react-intersection-observer"
+
 /*
     DESC: Displays all projects from projects.json.
 */
 export const Portfolio: React.FC = () => {
+    const [ref, inView] = useInView({
+        threshold: 0.1,
+        initialInView: true
+    })
+
     const projects: ProjectData[] = data;
 
     return (
-        <div className="flex">
+        <section ref={ref} className="flex">
             <div className="flex flex-grow flex-col items-center w-[22rem] md:w-[44rem] lg:w-[56rem] gap-10 drop-shadow-lg">
                 {projects.map((proj: ProjectData): JSX.Element => {
                     return (
                         <div key={proj.title} className="w-full">
-                            <Project {...proj} />
+                            <Project project={proj} portfolioView={inView} />
                         </div>
                     );
                 })}
             </div>
-        </div>
+        </section>
     );
 };
