cdf1835c02061a7efb7a3bbe3bed8c7e9c5db376
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-12 16:49:30 -0700

    Fixed font imports
    -Updated styles.css to import fonts correctly
    -Updated tailwind config to load fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
-      fontFamily: {
-        "metropolis": ["Metropolis", "sans-serif"]
-      },
       extend: {
-          colors: {
-              "white": "#F9F5EB",
-              "grey": "#E4DCCF",
-              "red": "#EA5455",
-              "blue": "#002B5B",
-              "lightblue": "#3F72AF"
-          }
+        fontFamily: {
+            "metropolis": ["Metropolis", "serif"]
+        },
+        colors: {
+            "white": "#F9F5EB",
+            "grey": "#E4DCCF",
+            "red": "#EA5455",
+            "blue": "#002B5B",
+            "lightblue": "#3F72AF"
+        }
       },
     },
     safelist: [
