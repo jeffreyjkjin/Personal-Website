683bc8cab1a2e785f689baee7ecb53de8bf1147b
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-23 01:55:29 -0700

    Updated ProjectTitle to use AccordionButton
    -Removed hard code for accordion button and replaced with AccordionButton
    -Removed flex-row-reverse class from flex box for buttons

--- src/project/ProjectTitle.tsx
+++ src/project/ProjectTitle.tsx
@@ -1,8 +1,6 @@
-import { AnimationControls, motion, useAnimation } from "framer-motion"
 import { ReactElement } from "react"
-import { MdExpandMore } from "react-icons/md"
 
-import { hover, tap } from "@/animation/Gestures.tsx"
+import { AccordionButton } from "@/buttons/AccordionButton.tsx"
 import { LinkButton } from "@/buttons/LinkButton.tsx"
 import { ProjectData } from "@/project/ProjectData.tsx"
 import "@/styles.css"
@@ -20,32 +18,19 @@ interface ProjectTitleProps {
            setOpen - A setter from Project; used to tell Project if the accordion is open or not.
 */
 export const ProjectTitle: React.FC<ProjectTitleProps> = ({ project, open, setOpen }) => {
-    const animation: AnimationControls = useAnimation();
 
     return (
         <div className="flex flex-grow font-metropolis text-2xl p-6 bg-grey">
             <div className="font-bold grow">
                 {project.title}
             </div>
-            <div className="flex flex-row-reverse justify-end gap-4 place-items-center">
-                <motion.button 
-                    onClick={() => {
-                        setOpen(!open)
-                        animation.start({
-                            rotate: open ? 0 : 180
-                        });
-                    }}
-                    whileHover={hover()}
-                    whileTap={tap()}
-                    animate={animation}
-                    >
-                    <MdExpandMore />
-                </motion.button>
+            <div className="flex justify-end gap-4 place-items-center">
                 {project.links && project.links.map((link: string): ReactElement => {
                     return (
                         <LinkButton link={link} key={link} />   
-                    );
-                })}
+                        );
+                    })}
+                    <AccordionButton open={open} setOpen={setOpen} />
             </div>
         </div>
     );
