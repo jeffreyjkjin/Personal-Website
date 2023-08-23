7aa77dae9c433ebb062034c501c1c3e4c85349bd
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-23 01:23:05 -0700

    Updated format of ProjectData
    -Replaced Github and Youtube properties with an array of links
    -Made image property optional
    -Updated placeholder project.json to adhere to new properties

--- src/project/ProjectData.tsx
+++ src/project/ProjectData.tsx
@@ -3,9 +3,8 @@
 */
 export interface ProjectData {
     title: string; 
-    github?: string;
-    youtube?: string; 
-    image: string;
+    links?: string[]; 
+    image?: string;
     tools: string[];   
     what: string;                
     soWhat: string[];
