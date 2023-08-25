5c3af69545c09e38ed0731fc37bea8b84b2923f6
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-25 02:25:28 -0700

    Created ScrollTopButton component
    -Returns user back to the top of the page on click
    -Appears when header is no longer visible
    -Added appear/exit animations

---
+++ src/buttons/ScrollTopButton.tsx
@@ -0,0 +1,51 @@
+import { AnimationControls, motion, useAnimation } from "framer-motion"
+import { useEffect } from "react"
+import { MdOutlineArrowUpward } from "react-icons/md"
+
+import { hover, tap } from "@/animation/Gestures.tsx"
+import "@/styles.css"
+
+interface ScrollTopButtonProps {
+    headerInView: boolean;
+}
+
+/*
+     DESC: A button that brings the user back to the top of the page.
+    PARAM: headerInView - Boolean that indicates whether the header is visible.
+*/
+export const ScrollTopButton: React.FC<ScrollTopButtonProps> = ({ headerInView }) => {
+    const animation: AnimationControls = useAnimation();
+
+    // show button when header is not visible
+    useEffect(() => {
+        animation.start({
+            y: !headerInView ? 0 : "10vh",
+            opacity: !headerInView ? 1 : 0,
+            transition: {
+                type: "spring",
+                bounce: 0.3
+            }
+        });
+    }, [headerInView])
+
+    return (
+        <motion.button
+            className="bg-blue p-3 rounded-full text-2xl text-white fixed bottom-[4rem] right-[4rem] lg:visible"
+            whileHover={hover()}
+            whileTap={tap()}
+            initial={{
+                y: "10vh",
+                opacity: 0
+            }}
+            animate={animation}
+            onClick={() => {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                })
+            }}
+            >
+            <MdOutlineArrowUpward />
+        </motion.button>
+    );
+};
