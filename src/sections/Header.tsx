95924c7b9a0fc325e76711c26e187361c538c977
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-08-25 02:27:16 -0700

    Added useInView hook to Header
    -Passed setHeaderInView method from App to grab the visibility of the header
    -Added useInView hook to the header
    -Used useEffect hook to update setHeaderInView

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,48 +1,59 @@
 import { AnimationControls, motion, useAnimation } from "framer-motion"
+import { useEffect } from "react"
+import { useInView } from "react-intersection-observer"
 
 import { Navbar } from "@/header/Navbar.tsx"
 import { Title } from "@/header/Title.tsx"
 import "@/styles.css"
 
 interface HeaderProps {
-    setClick: React.Dispatch<React.SetStateAction<string>>
+    setClick: React.Dispatch<React.SetStateAction<string>>;
+    setHeaderInView: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 /*
      DESC: Displays welcome message and navigation buttons.
     PARAM: setClick - A setter from App; used to find out what button was pressed by user in App.
+           setHeaderInView - A setter from App; used to find out if the header is visible.
 */
-export const Header: React.FC<HeaderProps> = ({ setClick }) => {
+export const Header: React.FC<HeaderProps> = ({ setClick, setHeaderInView }) => {
+    // passed to ScrollTopButton
+    const [ref, inView] = useInView({
+        threshold: 0,
+        initialInView: true        
+    });
+
+    useEffect(() => {
+        setHeaderInView(inView);
+    }, [inView]);
+
     const animation: AnimationControls = useAnimation();
 
-    /*
-        DESC: Animates navigational buttons from Navbar.
-    */
-    const animateNavbar = () => {
-        animation.start({
-            y: 0,
-            opacity: 1,
-            transition: {
-                type: "spring",
-                duration: 1,
-                bounce: 0.3   
-            }         
-        });
-    }
-
     return (
-        <div className="flex flex-col font-metropolis justify-items-center lg:justify-items-start h-screen w-[22rem] md:w-[44rem] lg:w-[56rem] place-content-center drop-shadow-lg">
-            <Title animateNavbar={animateNavbar} />
-            <motion.div 
-                className="flex text-lg gap-16 m-2 relative"
-                initial={{
-                    y: "10vh",
-                    opacity: 0
-                }}
-                animate={animation}
-                >
-                <Navbar setClick={setClick} />
-            </motion.div>
-        </div>
+        <section ref={ref}>
+            <div className="flex flex-col font-metropolis justify-items-center lg:justify-items-start h-screen w-[22rem] md:w-[44rem] lg:w-[56rem] place-content-center drop-shadow-lg">
+                <Title animateNavbar={() => {
+                    animation.start({
+                        y: 0,
+                        opacity: 1,
+                        transition: {
+                            type: "spring",
+                            duration: 1,
+                            bounce: 0.3
+                        }
+                    }); 
+                }} />
+                <motion.div 
+                    className="flex text-lg gap-16 m-2 relative"
+                    initial={{
+                        y: "10vh",
+                        opacity: 0
+                    }}
+                    animate={animation}
+                    >
+                    <Navbar setClick={setClick} />
+                </motion.div>
+            </div>
+        </section>
     );
-}
+};
