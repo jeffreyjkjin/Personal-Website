7bf716f399d7d99fd1b462ef45fa86e86ac3d16a
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-01-11 19:19:53 -0800

    Replaced useState setter parameter in ScrollTopButton with atom
    -Replaced with useState setter from headerInView atom
    -Deleted ScrollTopButtonProps interface
    -Updated animation and useEffect hook to use atom instead

--- src/buttons/ScrollTopButton.tsx
+++ src/buttons/ScrollTopButton.tsx
@@ -1,32 +1,32 @@
 import { AnimationControls, motion, useAnimation } from "framer-motion"
+import { useAtom } from "jotai"
 import { useEffect } from "react"
 import { MdOutlineArrowUpward } from "react-icons/md"
 
 import { hover, tap } from "@/animation/Gestures.tsx"
+import { headerInView } from "@/sections/Header.tsx"
 import "@/styles.css"
 
-interface ScrollTopButtonProps {
-    headerInView: boolean;
-}
 
 /*
      DESC: A button that brings the user back to the top of the page.
-    PARAM: headerInView - Boolean that indicates whether the header is visible.
 */
-export const ScrollTopButton: React.FC<ScrollTopButtonProps> = ({ headerInView }) => {
+export const ScrollTopButton: React.FC = () => {
+    const [inView] = useAtom(headerInView);
+    
     const animation: AnimationControls = useAnimation();
 
     // show button when header is not visible
     useEffect(() => {
         animation.start({
-            y: !headerInView ? 0 : "10vh",
-            opacity: !headerInView ? 1 : 0,
+            y: !inView ? 0 : "10vh",
+            opacity: !inView ? 1 : 0,
             transition: {
                 type: "spring",
                 bounce: 0.3
             }
         });
-    }, [headerInView])
+    }, [inView])
 
     return (
         <motion.button
