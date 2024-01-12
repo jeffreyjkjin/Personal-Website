05c1f194afeab7e1ce942db1bd5ac3a1e13cfe14
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-01-11 19:22:58 -0800

    Replaced useState hooks in App with atoms
    -Deleted headerInView useState hook
    -Replaced click useState with navbarOption atom
    -Replaced section reference hooks with options array
    -Updated useEffect hooks logic to use atom
    -Removed parameters from header call
    -Updated references of sections to use options array

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react"
+import { atom, useAtom } from "jotai"
+import { useEffect, useRef } from "react"
 
 import { ScrollTopButton } from "@/buttons/ScrollTopButton.tsx"
 import { About } from "@/sections/About.tsx"
@@ -8,63 +9,48 @@ import { Portfolio } from "@/sections/Portfolio.tsx"
 import { SectionCard } from "@/sections/SectionCard.tsx"
 import "@/styles.css"
 
+export const navbarOption = atom<number>(NaN);
+
 /*
     DESC: Displays each section on the website.
 */
 export const App: React.FC = () => {
-    // used to grab the visibility of the header and passed scroll to top button
-    const [headerInView, setHeaderInView] = useState<boolean>(true);
-
-    // logic for navbar buttons
-    const [click, setClick] = useState<string>("");
-
-    const about: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
-    const projects: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
-    const contact: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+    const [option, setOption] = useAtom(navbarOption);
     
+    const options: React.MutableRefObject<HTMLDivElement | null>[] = Array.from(
+        { length: 3 },
+        () => useRef(null)
+        );
+        
+    // logic for navbar
     useEffect(() => {
-        let section: React.MutableRefObject<HTMLDivElement | null>;
-        switch (click) {
-            case "about":
-                section = about;
-                break;
-            case "projects":
-                section = projects;
-                break;
-            case "contact":
-                section = contact;
-                break;
-            default:
-                return;
-        }
-
-        if (section.current) {
+        if (!isNaN(option)) {
             window.scrollTo({
-                top: section.current.offsetTop,
+                top: options[option].current?.offsetTop,
                 behavior: "smooth"
             });
 
-            setClick("");
+            setOption(NaN);
         }
-    }, [click]);
+    }, [option]);
 
     return (
         <div className="flex flex-col place-items-center">
-            <Header setClick={setClick} setHeaderInView={setHeaderInView} />
-            <ScrollTopButton headerInView ={headerInView} />
-            <div ref={about} className="flex my-[15vh] lg:my-[35vh] relative">
+            <Header />
+            <ScrollTopButton />
+            <div ref={options[0]} className="flex my-[15vh] lg:my-[35vh] relative">
                 <div className="absolute -left-[5rem]">
                     <SectionCard name={"About"} length={6} />
                 </div>
                 <About />
             </div>
-            <div ref={projects} className="flex my-[15vh] lg:my-[35vh] relative">
+            <div ref={options[1]} className="flex my-[15vh] lg:my-[35vh] relative">
                 <div className="absolute -left-[5rem]">
                     <SectionCard name={"Portfolio"} length={8} />
                 </div>
                 <Portfolio />
             </div>
-            <div ref={contact} className="flex my-[15vh] lg:my-[35vh] relative">
+            <div ref={options[2]} className="flex my-[15vh] lg:my-[35vh] relative">
                 <div className="absolute -left-[5rem]">
                     <SectionCard name={"Contact"} length={7.5} />
                 </div>
